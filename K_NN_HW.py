8461c08ca07f86464e77a1f5c3aced0c2e411c77
Aaronj88 <155629991+Aaronj88@users.noreply.github.com>
2025-03-05 17:57:39 +0100

    Update K_NN_HW.py

--- K_NN_HW.py
+++ K_NN_HW.py
@@ -8,32 +8,32 @@ i = pd.read_csv("German_Cars.csv")
 print(i.isnull().sum())
 print(i.info())
 
+#min-max scaling
+obj = MinMaxScaler()
+i[["Year","Mileage", 'Fuel Consumption (L/100km)','Horsepower (HP)']] = obj.fit_transform(i[["Year","Mileage", 'Fuel Consumption (L/100km)','Horsepower (HP)']])
+
+
+le = LabelEncoder()
+i['Brand'] = le.fit_transform(i['Brand'])
+i['Model'] = le.fit_transform(i['Model'])
+i['Fuel Type'] = le.fit_transform(i['Fuel Type'])
+i['Transmission'] = le.fit_transform(i['Transmission'])
+i['City'] = le.fit_transform(i['City'])
+
 import matplotlib.pyplot as plt
 import seaborn as sb
 sb.heatmap(i.corr(),annot=True)
 plt.show()
 
 print(i.columns)
-X = i.drop("Price ",axis=1)
+X = i.drop(["Price ","Brand","Model","City"],axis=1)
 y = i["Price "]/10000
 
-
-#min-max scaling
-obj = MinMaxScaler()
-X[["Year","Mileage", 'Fuel Consumption (L/100km)','Horsepower (HP)']] = obj.fit_transform(X[["Year","Mileage", 'Fuel Consumption (L/100km)','Horsepower (HP)']])
-
-
-le = LabelEncoder()
-X['Brand'] = le.fit_transform(X['Brand'])
-X['Model'] = le.fit_transform(X['Model'])
-X['Fuel Type'] = le.fit_transform(X['Fuel Type'])
-X['Transmission'] = le.fit_transform(X['Transmission'])
-X['City'] = le.fit_transform(X['City'])
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=8,train_size=0.8)
 
 
-model = KNeighborsRegressor(n_neighbors=5)
+model = KNeighborsRegressor(n_neighbors=3)
 model.fit(X_train,y_train)
 py = model.predict(X_test)
 
